98e29381a933ff211768ef1f8b1e8f145faeecc0
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-19 22:00:06 +0900

    :wrench: chore: mdx対応用の設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import pages from '@hono/vite-cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import client from 'honox/vite/client'
-import { defineConfig } from 'vite'
+import pages from "@hono/vite-cloudflare-pages";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import honox from "honox/vite";
+import client from "honox/vite/client";
+import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
+  if (mode === "client") {
     return {
-      plugins: [client()]
-    }
+      plugins: [client()],
+    };
   } else {
     return {
       plugins: [
         honox({
           devServer: {
-            adapter
-          }
+            adapter,
+          },
         }),
-        pages()
-      ]
-    }
+        pages(),
+        mdx({
+          jsxImportSource: "hono/jsx",
+          remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+        }),
+      ],
+    };
   }
-})
+});
