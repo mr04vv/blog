3f5668cfe2be404f2445c0a26c09937cfb93fbf0
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-19 22:06:24 +0900

    :recycle: chore: titleを変更

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,16 @@
-import { css } from 'hono/css'
-import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
+import { css } from "hono/css";
+import { createRoute } from "honox/factory";
 
 const className = css`
   font-family: sans-serif;
-`
+`;
 
 export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'Hono'
+  const name = "もりのブログ";
   return c.render(
     <div class={className}>
-      <h1>Hello, {name}!</h1>
-      <Counter />
+      <h1>{name}</h1>
     </div>,
     { title: name }
-  )
-})
+  );
+});
