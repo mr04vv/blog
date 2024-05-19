c1fd77f0b812f697bcb8ad772cf3feba66f24c24
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-19 21:50:05 +0900

    :sparkles: feat: create blog

---
+++ app/routes/_renderer.tsx
@@ -0,0 +1,19 @@
+import { Style } from 'hono/css'
+import { jsxRenderer } from 'hono/jsx-renderer'
+import { Script } from 'honox/server'
+
+export default jsxRenderer(({ children, title }) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <Script src="/app/client.ts" async />
+        <Style />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+})
