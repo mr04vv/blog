0f4e42fbc3e84612b32768465484df735d924db9
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-28 23:01:05 +0900

    :sparkles: feat: 記事一覧ページの仮実装

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,33 @@
-import { css } from "hono/css";
 import { createRoute } from "honox/factory";
-import { blogName } from "../constants";
-
-const className = css`
-  font-family: sans-serif;
-`;
+import { getPosts } from "../lib/posts";
 
 export default createRoute((c) => {
+  const posts = getPosts();
   return c.render(
-    <div class={className}>
-      <h1>{blogName}</h1>
+    <div class={"mt-6 flex flex-col gap-12"}>
+      {posts.map((post) => (
+        <div key={post.entryName} class={"flex flex-col"}>
+          <a href={`/entry/${post.entryName}`}>
+            <h2
+              class={
+                "transition text-2xl font-semibold text-[#4b4b4b] hover:underline"
+              }
+            >
+              {post.frontmatter.title}
+            </h2>
+          </a>
+
+          <div class={"gap-1 mt-2"}>
+            <time class={"text-gray-500 text-sm"}>
+              {new Date(post.frontmatter.date).toLocaleDateString("ja-JP")}
+            </time>
+            {" — "}
+            <span class={"text-gray-500 text-sm"}>
+              {post.frontmatter.description}
+            </span>
+          </div>
+        </div>
+      ))}
     </div>,
   );
 });
