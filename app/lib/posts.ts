727e91b17c4ea4f0fd224388e52b496cc152075f
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-28 14:06:53 +0900

    :sparkles: feat: 投稿の取得func追加

---
+++ app/lib/posts.ts
@@ -0,0 +1,29 @@
+import type { MDXProps } from "mdx/types";
+import type { Frontmatter } from "../types";
+import { getEntryNameFromPath } from "../utils";
+
+type MDX = {
+  frontmatter: Frontmatter;
+  default: (props: MDXProps) => JSX.Element;
+  ContentSummary?: () => JSX.Element;
+};
+
+const posts = import.meta.glob<MDX>("../routes/entry/**/*.mdx", {
+  eager: true,
+});
+
+export const getPosts = () => {
+  const postsData = Object.entries(posts).map(([path, post]) => {
+    const entryName = getEntryNameFromPath(path);
+    const { frontmatter } = post;
+    const { default: Component, ContentSummary } = post;
+    return { entryName, frontmatter, Component, ContentSummary };
+  });
+  return postsData;
+};
+
+export const getPostByEntryName = (entryName: string) => {
+  const posts = getPosts();
+  const post = posts.find((post) => post.entryName === entryName);
+  return post;
+};
