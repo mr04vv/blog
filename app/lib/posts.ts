0338116830af77696e994eac3da5dcac5f335f3b
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-28 23:00:20 +0900

    :sparkles: feat: 日付の新しい順にソートするように修正

--- app/lib/posts.ts
+++ app/lib/posts.ts
@@ -6,20 +6,34 @@ import { getEntryNameFromPath } from "../utils";
 type MDX = {
   frontmatter: Frontmatter;
   default: (props: MDXProps) => JSX.Element;
-  ContentSummary?: () => JSX.Element;
 };
 
 const posts = import.meta.glob<MDX>("../articles/**/*.mdx", {
   eager: true,
 });
 
+const sortByDateDesc = ():
+  | ((
+      a: [string, { frontmatter: Frontmatter }],
+      b: [string, { frontmatter: Frontmatter }],
+    ) => number)
+  | undefined => {
+  return ([_aid, aPost], [_bid, bPost]) => {
+    const aDate = new Date(aPost.frontmatter.date);
+    const bDate = new Date(bPost.frontmatter.date);
+    return aDate.getTime() < bDate.getTime() ? 1 : -1;
+  };
+};
+
 export const getPosts = () => {
-  const postsData = Object.entries(posts).map(([path, post]) => {
-    const entryName = getEntryNameFromPath(path);
-    const { frontmatter } = post;
-    const { default: Component, ContentSummary } = post;
-    return { entryName, frontmatter, Component, ContentSummary };
-  });
+  const postsData = Object.entries(posts)
+    .sort(sortByDateDesc())
+    .map(([path, post]) => {
+      const entryName = getEntryNameFromPath(path);
+      const { frontmatter } = post;
+      const { default: Component } = post;
+      return { entryName, frontmatter, Component };
+    });
   return postsData;
 };
 
