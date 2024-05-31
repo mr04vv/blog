8d025e17a602da25f1cfc6b83b1dd47c1f5dca5c
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-31 23:48:24 +0900

    :lipstick: feat: 記事一覧のコンポーネントを差し替え

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from "hono/jsx/jsx-runtime";
 import { createRoute } from "honox/factory";
+import { ArticleListItem } from "../components/articleListIte";
 import { getPosts } from "../lib/posts";
 
 export default createRoute((c) => {
@@ -6,24 +8,15 @@ export default createRoute((c) => {
   return c.render(
     <div class={"mt-6 flex flex-col gap-12"}>
       {posts.map((post) => (
-        <div
-          key={post.entryName}
-          class={"flex flex-col gap-1 dark:text-gray-100"}
-        >
-          <a href={`/entry/${post.entryName}`}>
-            <h2 class={"text-2xl font-semibold hover:underline"}>
-              {post.frontmatter.title}
-            </h2>
-          </a>
-          <div class={"flex flex-col gap-0.5"}>
-            <time class={"text-gray-500 dark:text-gray-400 text-sm"}>
-              {new Date(post.frontmatter.date).toLocaleDateString("ja-JP")}
-            </time>
-            <p class={"text-gray-500 dark:text-gray-400 text-sm"}>
-              {post.frontmatter.description}
-            </p>
-          </div>
-        </div>
+        <Fragment key={post.entryName}>
+          <ArticleListItem
+            date={post.frontmatter.date}
+            title={post.frontmatter.title}
+            description={post.frontmatter.description}
+            iconUrl={post.frontmatter.iconUrl}
+            entryName={post.entryName}
+          />
+        </Fragment>
       ))}
     </div>,
   );
