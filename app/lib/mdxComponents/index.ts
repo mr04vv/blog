bda382d30f5e01d6f39df3bffbe01496670ab2a0
Takuto Mori Gump <mr04vv@gmail.com>
2024-06-01 00:37:39 +0900

    :recycle: chore: titleIconをmdxから剥がした

--- app/lib/mdxComponents/index.ts
+++ app/lib/mdxComponents/index.ts
@@ -1,13 +1,11 @@
 import type { MDXComponents } from "mdx/types";
 import { ArticleImage } from "./articleImage";
 import { ExternalOgp } from "./externalOgp";
-import { TitleIcon } from "./titleIcon";
 
 export function useMDXComponents(): MDXComponents {
   const components = {
     img: ArticleImage,
     ExternalOgp: ExternalOgp,
-    TitleIcon: TitleIcon,
   };
   return components;
 }
