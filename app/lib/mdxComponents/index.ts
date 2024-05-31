ff7b109d55145b20aa27ebbf34ac926568497f31
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-31 23:48:39 +0900

    :sparkles: feat: 記事のアイコンを設定

--- app/lib/mdxComponents/index.ts
+++ app/lib/mdxComponents/index.ts
@@ -1,11 +1,13 @@
 import type { MDXComponents } from "mdx/types";
 import { ArticleImage } from "./articleImage";
 import { ExternalOgp } from "./externalOgp";
+import { TitleIcon } from "./titleIcon";
 
 export function useMDXComponents(): MDXComponents {
   const components = {
     img: ArticleImage,
     ExternalOgp: ExternalOgp,
+    TitleIcon: TitleIcon,
   };
   return components;
 }
