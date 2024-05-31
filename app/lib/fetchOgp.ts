8a78428ddcbf08b3e820405f8e00a8c2ed23d4bf
Takuto Mori Gump <mr04vv@gmail.com>
2024-05-31 10:54:53 +0900

    :sparkles: feat: ogp取得する関数を追加

---
+++ app/lib/fetchOgp.ts
@@ -0,0 +1,49 @@
+import { JSDOM } from "jsdom";
+type OgpKey = "title" | "description" | "image" | "url";
+type Ogp = {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+  imageAlt?: string;
+  favicon?: string;
+};
+export const fetchOgp = async (url: string) => {
+  const ogp: Ogp = {
+    title: "",
+    description: "",
+    image: "",
+    url: "",
+  };
+  try {
+    const dom = await JSDOM.fromURL(url);
+    const host = new URL(url).host;
+    ogp.favicon = `https://www.google.com/s2/favicons?domain=${host}&sz=20`;
+    const metas = dom.window.document.getElementsByTagName("meta");
+
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    Array.from(metas).forEach((v) => {
+      const prop = v.getAttribute("property");
+      if (!prop) return;
+      const key = prop.replace("og:", "");
+      if (key === "image:alt") ogp.imageAlt = v.getAttribute("content") || "";
+      if (!isOgpKey(key)) return;
+      ogp[key] = v.getAttribute("content") || "";
+    });
+
+    return ogp;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function isOgpKey(key: any): key is OgpKey {
+  return (
+    key === "title" ||
+    key === "image" ||
+    key === "description" ||
+    key === "url" ||
+    key === "alt"
+  );
+}
