8027f51e3f74604ac271d6e6966c9a3a316ddd1b
Takuto Mori Gump <mr04vv@gmail.com>
2024-06-01 15:08:56 +0900

    🩹 fix: 脚注の戻る動作がうまくいってなかったやつの修正

--- app/lib/mdxComponents/anchorLink.tsx
+++ app/lib/mdxComponents/anchorLink.tsx
@@ -1,10 +1,18 @@
-export const AnchorLink = (props: { href: string; children: string }) => (
-  <a
-    href={props.href}
-    target="_blank"
-    class={"text-blue-500 dark:text-blue-400 underline mx-1"}
-    rel="noreferrer"
-  >
-    {props.children}
-  </a>
-);
+export const AnchorLink = ({
+  href,
+  children,
+  className,
+  ...rest
+}: { href: string; children: string; className?: string }) => {
+  return (
+    <a
+      href={href}
+      {...rest}
+      className={`text-blue-500 dark:text-blue-400 underline mx-1 ${
+        className ? className : ""
+      }`}
+    >
+      {children}
+    </a>
+  );
+};
