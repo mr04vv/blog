24e93dd3bfbc8ce4e34e2869b4be43d659e517df
Takuto Mori Gump <mr04vv@gmail.com>
2024-06-01 13:06:47 +0900

    🩹 chore: androidのbfc対策を追加

--- app/theme.ts
+++ app/theme.ts
@@ -1,10 +1,17 @@
 // レンダリングされる前に実行しないとチラつきが発生する
-const currentTheme = localStorage.getItem("theme");
-const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-if (currentTheme === null) {
-  if (isDark) {
-    document.documentElement.classList.add("dark");
+const theme = () => {
+  const currentTheme = localStorage.getItem("theme");
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (currentTheme === null) {
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+    }
+  } else {
+    if (currentTheme === "dark") document.documentElement.classList.add("dark");
   }
-} else {
-  if (currentTheme === "dark") document.documentElement.classList.add("dark");
-}
+};
+
+theme();
+// androidのブラウザバックのback forward cache対策
+window.addEventListener("pagehide", () => theme());
+window.addEventListener("pageshow", () => theme());
