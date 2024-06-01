5dd211ada4ee5074ae4fae988b7e1e0f47781db3
Takuto Mori Gump <mr04vv@gmail.com>
2024-06-01 23:42:19 +0900

    :recycle: chore: mdx内のaタグの挙動調整

--- app/lib/mdxComponents/anchorLink.tsx
+++ app/lib/mdxComponents/anchorLink.tsx
@@ -2,8 +2,15 @@ export const AnchorLink = ({
   href,
   children,
   className,
+  "data-footnote-ref": isFootnoteRef,
   ...rest
-}: { href: string; children: string; className?: string }) => {
+}: {
+  href: string;
+  children: string;
+  className?: string;
+  "data-footnote-ref": string;
+}) => {
+  const isFootNoteBackRef = className === "data-footnote-backref";
   return (
     <a
       href={href}
@@ -11,6 +18,8 @@ export const AnchorLink = ({
       className={`text-blue-500 dark:text-blue-400 underline mx-1 ${
         className ? className : ""
       }`}
+      target={isFootNoteBackRef || isFootnoteRef ? undefined : "_blank"}
+      rel="noreferrer"
     >
       {children}
     </a>
