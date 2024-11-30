7d574a446850ca4cb99d2d9ca6289b03fce70a3e
Takuto Mori Gump <mr04vv@gmail.com>
2024-11-30 17:11:17 +0900

    🔧 chore: prodの判定方法を修正

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -22,7 +22,7 @@ export default jsxRenderer(({ children, title, entryName, frontmatter }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
-        {import.meta.env.PROD ? (
+        {import.meta.env.MODE === "production" ? (
           <meta
             property="og:url"
             content={`https://blog.mooriii.com${pagePath}`}
@@ -34,7 +34,7 @@ export default jsxRenderer(({ children, title, entryName, frontmatter }) => {
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={description} />
         <meta property="og:site_name" content={blogName} />
-        {import.meta.env.PROD ? (
+        {import.meta.env.MODE === "production" ? (
           <meta
             property="og:image"
             content={`https://blog.mooriii.com${ogpPath}`}
@@ -46,7 +46,7 @@ export default jsxRenderer(({ children, title, entryName, frontmatter }) => {
         <meta name="twitter:site" content="@mooriii" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={description} />
-        {import.meta.env.PROD ? (
+        {import.meta.env.MODE === "production" ? (
           <meta
             name="twitter:image"
             content={`https://blog.mooriii.com${ogpPath}`}
@@ -55,19 +55,19 @@ export default jsxRenderer(({ children, title, entryName, frontmatter }) => {
           <meta name="twitter:image" content={ogpPath} />
         )}
         <title>{pageTitle}</title>
-        {import.meta.env.PROD ? (
+        {import.meta.env.MODE === "production" ? (
           <link rel="icon" href="https://blog.mooriii.com/favicon.ico" />
         ) : (
           <link rel="icon" href="/favicon.ico" />
         )}
-        {import.meta.env.PROD ? (
+        {import.meta.env.MODE === "production" ? (
           <script src="/static/theme.js" />
         ) : (
           <script src="/app/theme.ts" />
         )}
         <Script src="/app/client.ts" async />
         <Style />
-        {import.meta.env.PROD ? (
+        {import.meta.env.MODE === "production" ? (
           <link href="/styles/style.css" rel="stylesheet" />
         ) : (
           <link href={styles} rel="stylesheet" />
