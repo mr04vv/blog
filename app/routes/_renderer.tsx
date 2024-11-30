b2f12148509ca7646b4b19bf2debd415809fdcd5
Takuto Mori Gump <mr04vv@gmail.com>
2024-11-30 17:47:50 +0900

    Revert ":sparkles: feat: 相対パスに変更"

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -10,8 +10,7 @@ export default jsxRenderer(({ children, title, entryName, frontmatter }) => {
   const pageTitle = title ? `${title} - ${blogName}` : blogName;
   const ogpPath = title ? `/ogps/${entryName}.png` : "/ogp.png";
   const c = useRequestContext();
-  const pagePath = c.req.path;
-  console.log("pagePath", import.meta.env, pagePath);
+  const currentUrl = c.req.url;
   const description =
     frontmatter?.description ?? "日常や技術に関して気まぐれに投稿する日記";
 
@@ -22,52 +21,37 @@ export default jsxRenderer(({ children, title, entryName, frontmatter }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
-        {import.meta.env.MODE === "production" ? (
-          <meta
-            property="og:url"
-            content={`https://blog.mooriii.com${pagePath}`}
-          />
-        ) : (
-          <meta property="og:url" content={pagePath} />
-        )}
+        <meta property="og:url" content={currentUrl} />
         <meta property="og:type" content="article" />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={description} />
         <meta property="og:site_name" content={blogName} />
-        {import.meta.env.MODE === "production" ? (
-          <meta
-            property="og:image"
-            content={`https://blog.mooriii.com${ogpPath}`}
-          />
-        ) : (
-          <meta property="og:image" content={ogpPath} />
-        )}
+        <meta
+          property="og:image"
+          content={`https://blog.mooriii.com${ogpPath}`}
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@mooriii" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={description} />
-        {import.meta.env.MODE === "production" ? (
-          <meta
-            name="twitter:image"
-            content={`https://blog.mooriii.com${ogpPath}`}
-          />
-        ) : (
-          <meta name="twitter:image" content={ogpPath} />
-        )}
+        <meta
+          name="twitter:image"
+          content={`https://blog.mooriii.com${ogpPath}`}
+        />
         <title>{pageTitle}</title>
-        {import.meta.env.MODE === "production" ? (
+        {import.meta.env.PROD ? (
           <link rel="icon" href="https://blog.mooriii.com/favicon.ico" />
         ) : (
           <link rel="icon" href="/favicon.ico" />
         )}
-        {import.meta.env.MODE === "production" ? (
+        {import.meta.env.PROD ? (
           <script src="/static/theme.js" />
         ) : (
           <script src="/app/theme.ts" />
         )}
         <Script src="/app/client.ts" async />
         <Style />
-        {import.meta.env.MODE === "production" ? (
+        {import.meta.env.PROD ? (
           <link href="/styles/style.css" rel="stylesheet" />
         ) : (
           <link href={styles} rel="stylesheet" />
