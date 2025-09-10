fb9067f452164f828abdd7c7e587c2b40d22af02
Takuto Mori Gump <mr04vv@gmail.com>
2025-09-11 01:08:13 +0900

    :wrench: chore: migrate biome

--- app/islands/cursor-demo.tsx
+++ app/islands/cursor-demo.tsx
@@ -37,7 +37,7 @@ export const CursorDemo = () => {
 
   const memoized = useMemo(() => debounce(debouncedChangeInterval, 300), []);
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  // biome-ignore lint/suspicious/noExplicitAny: Event type from range input
   const handleChangeRange = (e: any) => {
     const val = e.target.value;
     setThrottleInterval(val);
@@ -126,9 +126,9 @@ const Cursor = ({ x, y, isAnimate }: Props) => {
         transform: `translate(${x}px, ${y}px)`,
       }}
     >
-      {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+      {/* biome-ignore lint/a11y/noSvgWithoutTitle: Decorative cursor icon */}
       <svg width="32" height="44" viewBox="0 0 24 36" fill="none">
-        {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+        {/* biome-ignore lint/style/useSelfClosingElements: SVG path element */}
         <path
           fill="#3AABFF"
           d="M0.928548 2.18278C0.619075 1.37094 1.42087 0.577818 2.2293 0.896107L14.3863 5.68247C15.2271 6.0135 15.2325 7.20148 14.3947 7.54008L9.85984 9.373C9.61167 9.47331 9.41408 9.66891 9.31127 9.91604L7.43907 14.4165C7.09186 15.2511 5.90335 15.2333 5.58136 14.3886L0.928548 2.18278Z"
